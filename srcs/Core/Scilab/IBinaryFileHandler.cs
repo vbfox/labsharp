6fcf3f244804b2d38ada414396e008ccdb1cf052
Roncaglia Julien <julien@roncaglia.fr>
2010-01-14 22:55:39 +0100

    Added the SAX-Like Scilab binary file reader.
    The reader is only able to read floating point matrices (real and comple).
    xUnit tests are provided to test the reading of some simple files.

---
+++ srcs/Core/Scilab/IBinaryFileHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace LabSharp.Scilab
+{
+    public interface IBinaryFileHandler
+    {
+        void FloatMatrix(string name, int rows, int columns, bool isComplex, BinaryReader reader);
+    }
+}
---
+++ tests/CoreTests/Scilab/BinaryFileReaderTest.cs
@@ -0,0 +1,87 @@
+﻿namespace LabSharp.Tests.Scilab
+{
+    #region using
+    using System.IO;
+    using LabSharp.Scilab;
+    using Xunit;
+    #endregion
+
+    public class BinaryFileReaderTest
+    {
+        class TestBinaryFileHandler : IBinaryFileHandler
+        {
+            public delegate void FloatMatrixDelegate(string name, int rows, int columns, bool isComplex, BinaryReader reader);
+
+            public event FloatMatrixDelegate OnFloatMatrix;
+
+            public void FloatMatrix(string name, int rows, int columns, bool isComplex, BinaryReader reader)
+            {
+                if (OnFloatMatrix != null)
+                {
+                    OnFloatMatrix(name, rows, columns, isComplex, reader);
+                }
+            }
+        }
+
+        [Fact]
+        void CouldReadFloatMatrix()
+        {
+            // Test with [1 2; 3 4]
+            var reader = new BinaryFileReader();
+            var stream = new MemoryStream(TestFiles.float_matrix);
+            var handler = new TestBinaryFileHandler();
+
+            var floatMatrixCount = 0;
+            handler.OnFloatMatrix += (string name, int rows, int columns, bool isComplex, BinaryReader br) =>
+                {
+                    Assert.Equal("float_matrix", name);
+                    Assert.Equal(2, rows);
+                    Assert.Equal(2, rows);
+                    Assert.Equal(false, isComplex);
+                    Assert.NotNull(reader);
+
+                    Assert.Equal(1, br.ReadDouble());
+                    Assert.Equal(3, br.ReadDouble());
+                    Assert.Equal(2, br.ReadDouble());
+                    Assert.Equal(4, br.ReadDouble());
+                    floatMatrixCount++;
+                };
+
+            reader.Parse(stream, handler);
+            Assert.Equal(1, floatMatrixCount);
+        }
+
+        [Fact]
+        void CouldReadComplexMatrix()
+        {
+            // Test with [1+1*%i 2+2*%i; 3+3*%i 4+4*%i]
+            var reader = new BinaryFileReader();
+            var stream = new MemoryStream(TestFiles.complex_matrix);
+            var handler = new TestBinaryFileHandler();
+
+            var floatMatrixCount = 0;
+            handler.OnFloatMatrix += (string name, int rows, int columns, bool isComplex, BinaryReader br) =>
+            {
+                Assert.Equal("complex_matrix", name);
+                Assert.Equal(2, rows);
+                Assert.Equal(2, rows);
+                Assert.Equal(true, isComplex);
+                Assert.NotNull(reader);
+                
+                Assert.Equal(1, br.ReadDouble());
+                Assert.Equal(3, br.ReadDouble());
+                Assert.Equal(2, br.ReadDouble());
+                Assert.Equal(4, br.ReadDouble());
+
+                Assert.Equal(1, br.ReadDouble());
+                Assert.Equal(3, br.ReadDouble());
+                Assert.Equal(2, br.ReadDouble());
+                Assert.Equal(4, br.ReadDouble());
+                floatMatrixCount++;
+            };
+
+            reader.Parse(stream, handler);
+            Assert.Equal(1, floatMatrixCount);
+        }
+    }
+}
