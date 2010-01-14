3514101641048cac47850de466219c9edafcd640
Roncaglia Julien <julien@roncaglia.fr>
2010-01-14 23:03:09 +0100

    Add an xUnit test with a file containing two float variables
    One variable is complex and the other one isn't to test for synchronization
    problems in the file reader.

--- tests/CoreTests/Scilab/BinaryFileReaderTest.cs
+++ tests/CoreTests/Scilab/BinaryFileReaderTest.cs
@@ -34,21 +34,26 @@
             var floatMatrixCount = 0;
             handler.OnFloatMatrix += (string name, int rows, int columns, bool isComplex, BinaryReader br) =>
                 {
-                    Assert.Equal("float_matrix", name);
-                    Assert.Equal(2, rows);
-                    Assert.Equal(2, rows);
-                    Assert.Equal(false, isComplex);
-                    Assert.NotNull(reader);
-
-                    Assert.Equal(1, br.ReadDouble());
-                    Assert.Equal(3, br.ReadDouble());
-                    Assert.Equal(2, br.ReadDouble());
-                    Assert.Equal(4, br.ReadDouble());
+                    AssertFloatMatrix(name, rows, isComplex, br);
                     floatMatrixCount++;
                 };
 
             reader.Parse(stream, handler);
             Assert.Equal(1, floatMatrixCount);
+        }
+
+        private static void AssertFloatMatrix(string name, int rows, bool isComplex, BinaryReader br)
+        {
+            Assert.Equal("float_matrix", name);
+            Assert.Equal(2, rows);
+            Assert.Equal(2, rows);
+            Assert.Equal(false, isComplex);
+            Assert.NotNull(br);
+
+            Assert.Equal(1, br.ReadDouble());
+            Assert.Equal(3, br.ReadDouble());
+            Assert.Equal(2, br.ReadDouble());
+            Assert.Equal(4, br.ReadDouble());
         }
 
         [Fact]
@@ -62,26 +67,60 @@
             var floatMatrixCount = 0;
             handler.OnFloatMatrix += (string name, int rows, int columns, bool isComplex, BinaryReader br) =>
             {
-                Assert.Equal("complex_matrix", name);
-                Assert.Equal(2, rows);
-                Assert.Equal(2, rows);
-                Assert.Equal(true, isComplex);
-                Assert.NotNull(reader);
-                
-                Assert.Equal(1, br.ReadDouble());
-                Assert.Equal(3, br.ReadDouble());
-                Assert.Equal(2, br.ReadDouble());
-                Assert.Equal(4, br.ReadDouble());
-
-                Assert.Equal(1, br.ReadDouble());
-                Assert.Equal(3, br.ReadDouble());
-                Assert.Equal(2, br.ReadDouble());
-                Assert.Equal(4, br.ReadDouble());
+                AssertComplexMatrix(name, rows, isComplex, br);
                 floatMatrixCount++;
             };
 
             reader.Parse(stream, handler);
             Assert.Equal(1, floatMatrixCount);
+        }
+
+        private static void AssertComplexMatrix(string name, int rows, bool isComplex, BinaryReader br)
+        {
+            Assert.Equal("complex_matrix", name);
+            Assert.Equal(2, rows);
+            Assert.Equal(2, rows);
+            Assert.Equal(true, isComplex);
+            Assert.NotNull(br);
+
+            Assert.Equal(1, br.ReadDouble());
+            Assert.Equal(3, br.ReadDouble());
+            Assert.Equal(2, br.ReadDouble());
+            Assert.Equal(4, br.ReadDouble());
+
+            Assert.Equal(1, br.ReadDouble());
+            Assert.Equal(3, br.ReadDouble());
+            Assert.Equal(2, br.ReadDouble());
+            Assert.Equal(4, br.ReadDouble());
+        }
+
+        [Fact]
+        void CouldReadMultiVariableFile()
+        {
+            var reader = new BinaryFileReader();
+            var stream = new MemoryStream(TestFiles.complex_and_float_matrix);
+            var handler = new TestBinaryFileHandler();
+
+            var floatMatrixCount = 0;
+            var complexMatrixCount = 0;
+            handler.OnFloatMatrix += (string name, int rows, int columns, bool isComplex, BinaryReader br) =>
+            {
+                if (name == "complex_matrix")
+                {
+                    AssertComplexMatrix(name, rows, isComplex, br);
+                    complexMatrixCount++;
+                }
+                else if (name == "float_matrix")
+                {
+                    AssertFloatMatrix(name, rows, isComplex, br);
+                    floatMatrixCount++;
+                }
+                
+            };
+
+            reader.Parse(stream, handler);
+            Assert.Equal(1, floatMatrixCount);
+            Assert.Equal(1, complexMatrixCount);
         }
     }
 }
